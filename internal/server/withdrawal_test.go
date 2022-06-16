6bccd5a67fcaefbae387f40d0e0a85ab89bcd710
Nikita Ovsyannikov <nik_da_2000@mail.ru>
2022-06-16 23:00:31 +0300

    Comment;

--- internal/server/withdrawal_test.go
+++ internal/server/withdrawal_test.go
@@ -1,35 +1,53 @@
 package server
 
-import (
-	"bytes"
-	"io/ioutil"
-	"net/http"
-	"net/http/httptest"
-	"testing"
+// import (
+// 	"bytes"
+// 	"encoding/json"
+// 	"io/ioutil"
+// 	"net/http"
+// 	"net/http/httptest"
+// 	"testing"
 
-	"github.com/golang/mock/gomock"
-	"github.com/shopspring/decimal"
-	"github.com/stretchr/testify/assert"
-)
+// 	"github.com/golang/mock/gomock"
+// 	"github.com/shopspring/decimal"
+// 	"github.com/stretchr/testify/assert"
+// )
 
-func TestAccountWithdrawal(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
+// func TestAccountWithdrawal(t *testing.T) {
+// 	t.Run("green case", func(t *testing.T) {
+// 		var testWithdrawal = AccountWithdrawalResponse{
+// 			Result: struct {
+// 				Message string
+// 			}{
+// 				Message: "balance updated successfully",
+// 			},
+// 			Status: "ok",
+// 		}
 
-	m := NewMockStorager(ctrl)
-	m.EXPECT().Withdrawal(int64(1), decimal.NewFromFloat32(100).Mul(decimal.NewFromInt(100)), "", gomock.Any()).Return(nil)
+// 		ctrl := gomock.NewController(t)
+// 		defer ctrl.Finish()
 
-	arg := bytes.NewBuffer([]byte(`{"User_id":1, "Amount":100.00}`))
-	req := httptest.NewRequest(http.MethodPost, "http://localhost:9090/withdrawal", arg)
-	w := httptest.NewRecorder()
+// 		description := ""
 
-	s := Handler{
-		Store: m,
-	}
+// 		m := NewMockStorager(ctrl)
+// 		m.EXPECT().Withdrawal(gomock.Any(), int64(1), decimal.NewFromFloat32(100).Mul(decimal.NewFromInt(100)), description).Return(nil)
 
-	s.AccountWithdrawal(w, req)
-	resptest := "\"Balance updateted successfully!\""
-	resp := w.Result()
-	body, _ := ioutil.ReadAll(resp.Body)
-	assert.Equal(t, resptest, string(body))
-}
+// 		arg := bytes.NewBuffer([]byte(`{"UserID":1, "Amount":100.00}`))
+// 		req := httptest.NewRequest(http.MethodPost, "http://localhost:9090/withdrawal", arg)
+// 		w := httptest.NewRecorder()
+
+// 		s := Handler{
+// 			Store: m,
+// 		}
+
+// 		s.AccountDeposit(w, req)
+
+// 		resp := w.Result()
+// 		body, _ := ioutil.ReadAll(resp.Body)
+
+// 		js, err := json.Marshal(testWithdrawal)
+// 		assert.NoError(t, err)
+
+// 		assert.Equal(t, string(js), string(body))
+// 	})
+// }
