521775aecfeb28952c1ad5ce45aa91c451d07526
Nikita Ovsyannikov <nik_da_2000@mail.ru>
2022-06-16 22:54:09 +0300

    Сorrection of fields of structures

--- internal/storage/entities.go
+++ internal/storage/entities.go
@@ -7,23 +7,23 @@ import (
 )
 
 type UserBalance struct {
-	AccountID int64           `json:"UserID"`
-	Balance   decimal.Decimal `json:"Balance"`
+	AccountID int64           `json:"userID"`
+	Balance   decimal.Decimal `json:"balance"`
 }
 
 type ReadUserHistoryResult struct {
-	AcountID    int64           `json:"userID"`
-	CBjournal   string          `json:"cbJournal"`
+	AccountID   int64           `json:"userID"`
+	CBjournal   OperationType   `json:"cbJournal"`
 	Amount      decimal.Decimal `json:"amount"`
 	Date        time.Time       `json:"date"`
 	Addressee   *int64          `json:"addressee"`
 	Description *string         `json:"description"`
 }
 
-type operationType string
+type OperationType string
 
 const (
-	operationTypeDeposit    operationType = "deposit"
-	operationTypeWithdrawal operationType = "withdrawal"
-	operationTypeTransfer   operationType = "transfer"
+	OperationTypeDeposit    OperationType = "deposit"
+	OperationTypeWithdrawal OperationType = "withdrawal"
+	OperationTypeTransfer   OperationType = "transfer"
 )
