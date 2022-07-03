710430c446fb656214b8fe8904040df9d68bada3
Nikita Ovsyannikov <nik_da_2000@mail.ru>
2022-07-03 17:41:39 +0300

    remove custom unmarshal to the separate file

---
+++ internal/storage/order.go
@@ -0,0 +1,27 @@
+package storage
+
+import (
+	"encoding/json"
+	"errors"
+)
+
+var ErrBadOrderType = errors.New("wrong value of ordBy type")
+
+func (j *OrdBy) UnmarshalJSON(v []byte) error {
+	var s string
+
+	if err := json.Unmarshal(v, &s); err != nil {
+		return err
+	}
+
+	switch s {
+	case string(OrderByAmount):
+		*j = OrderByAmount
+	case string(OrderByDate):
+		*j = OrderByDate
+	default:
+		return ErrBadOrderType
+	}
+
+	return nil
+}
