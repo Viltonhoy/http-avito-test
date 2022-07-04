d04df2f09ee4fc3bad14708603eb3dbf788d2bc9
Nikita Ovsyannikov <nik_da_2000@mail.ru>
2022-07-04 11:46:23 +0300

    adding a roll-up table system to more efficiently obtain the balance value;
    removed the materialized view system;
    
    minor bug fixes;

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -3,9 +3,10 @@ package storage
 import (
 	"context"
 	"errors"
-	"fmt"
 	"time"
 
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pgerrcode"
 	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/log/zapadapter"
 	"github.com/jackc/pgx/v4/pgxpool"
@@ -20,6 +21,30 @@ type Storage struct {
 }
 
 const cacheBookAccountID = int64(0)
+
+const updateRollUpTable = `
+	with var1 as (
+	select id from posting where account_id = $1 order by id desc limit 1
+	), var2 as(
+	select coalesce(sum(amount),0) from posting where account_id = $1 and id > (select coalesce((select last_tx_id from balances where account_id = $1),0))
+	) insert into balances (
+	balance,
+	account_id,
+	last_tx_id
+	) values (
+	(select * from var2),
+	$1,
+	(select * from var1)
+	) on conflict (account_id) do update
+	set last_tx_id = (select * from var1),
+	balance = (select * from var2) + (select balance from balances where account_id = $1) returning balance`
+
+var ErrUserAvailability = errors.New("sender does not exist")
+
+var (
+	ErrWithdrawal = errors.New("not enough money to withdraw")
+	ErrTransfer   = errors.New("not enough money to transfer")
+)
 
 // NewStore constructs Store instance with configured logger
 func NewStorage(ctx context.Context, logger *zap.Logger) (*Storage, error) {
@@ -36,13 +61,13 @@ func NewStorage(ctx context.Context, logger *zap.Logger) (*Storage, error) {
 	// create a pool connection
 	pool, err := pgxpool.ConnectConfig(ctx, config)
 	if err != nil {
-		logger.Error("cannot connect using config", zap.Error(err))
+		logger.Error("error database connection", zap.Error(err))
 		return nil, err
 	}
 
 	err = pool.Ping(ctx)
 	if err != nil {
-		logger.Error("connection is lost", zap.Error(err))
+		logger.Error("connection was not established", zap.Error(err))
 		return &Storage{}, err
 	}
 
@@ -59,47 +84,47 @@ func (s *Storage) Close() {
 }
 
 //ReadUser reads user's balance and returns it's id and balance
-func (s *Storage) ReadUser(ctx context.Context, user_id int64) (u UserBalance, err error) {
-	s.Logger.Debug("reading the user balance", zap.Int64("userID", user_id))
+func (s *Storage) ReadUserByID(ctx context.Context, userID int64) (u User, err error) {
+	logger := s.Logger.With(zap.Int64("user_ID", userID))
+	logger.Debug("reading the user balance")
 
 	tx, err := s.DB.Begin(ctx)
 	if err != nil {
-		return UserBalance{}, err
+		return User{}, err
 	}
 
-	refreshSql := `REFRESH MATERIALIZED VIEW account_balances;`
-
-	_, err = tx.Exec(ctx, refreshSql)
-	if err != nil {
-		tx.Rollback(ctx)
-		s.Logger.Error("failed to refresh materialized view account_balances", zap.Error(err))
-		return UserBalance{}, err
-	}
-
-	selectSql := `SELECT balance FROM account_balances WHERE user_id = $1;`
+	defer func() {
+		if err != nil {
+			if errRollback := tx.Rollback(ctx); errRollback != nil {
+				logger.Error("error rolls back the transaction", zap.Error(err))
+			}
+		}
+	}()
 
 	//query execution
-	err = tx.QueryRow(ctx, selectSql, user_id).Scan(&u.Balance)
+	err = tx.QueryRow(ctx, updateRollUpTable, userID).Scan(&u.Balance)
 	if err != nil {
-		tx.Rollback(ctx)
-		s.Logger.Error("cannot return user with specified id")
-		if errors.Is(err, pgx.ErrNoRows) {
-			return u, errors.New("user does not exist ")
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) && pgErr.Code == pgerrcode.NotNullViolation {
+			logger.Error("error returning user balance with specified id: user does not exist", zap.Error(err))
+			return User{}, ErrUserAvailability
 		}
-		return UserBalance{}, err
+		logger.Error("error returning user balance with specified id", zap.Error(err))
+		return User{}, err
 	}
 
-	u.AccountID = user_id
+	u.AccountID = userID
 
 	err = tx.Commit(ctx)
-	return UserBalance{
+	return User{
 		u.AccountID,
 		u.Balance,
 	}, err
 }
 
-func (s *Storage) Deposit(ctx context.Context, user_id int64, amount decimal.Decimal) (err error) {
-	s.Logger.Debug("money deposit", zap.Int64(`userID`, user_id), zap.String(`amount`, amount.String()))
+func (s *Storage) Deposit(ctx context.Context, userID int64, amount decimal.Decimal) (err error) {
+	logger := s.Logger.With(zap.Int64(`user_ID`, userID))
+	logger.Debug("money deposit")
 
 	var now = time.Now()
 
@@ -108,47 +133,54 @@ func (s *Storage) Deposit(ctx context.Context, user_id int64, amount decimal.Dec
 		return err
 	}
 
-	firstInsertSql := `INSERT INTO posting (account_id, cb_journal, accounting_period, amount, date)
+	defer func() {
+		if err != nil {
+			if errRollback := tx.Rollback(ctx); errRollback != nil {
+				logger.Error("error rolls back the transaction", zap.Error(err))
+			}
+		}
+	}()
+
+	firstInsertExec := `INSERT INTO posting (account_id, cb_journal, accounting_period, amount, date)
 			VALUES ($1, $4, $5, $2, $3);`
 
 	_, err = tx.Exec(
 		ctx,
-		firstInsertSql,
-		user_id,
+		firstInsertExec,
+		userID,
 		amount,
 		now.Format(time.RFC3339),
 		OperationTypeDeposit,
-		fmt.Sprintf(`Period: %d`, now.Year()),
+		now,
 	)
 	if err != nil {
-		tx.Rollback(ctx)
-		s.Logger.Error("failed to insert record", zap.Error(err))
+		logger.Error("failed to insert record", zap.Error(err))
 		return err
 	}
 
-	secondInsertSql := `INSERT INTO posting (account_id, cb_journal, accounting_period, amount, date)
+	secondInsertExec := `INSERT INTO posting (account_id, cb_journal, accounting_period, amount, date)
 			VALUES ($5, $3, $4, -1 * $1, $2);`
 
 	_, err = tx.Exec(
 		ctx,
-		secondInsertSql,
+		secondInsertExec,
 		amount,
 		now.Format(time.RFC3339),
 		OperationTypeDeposit,
-		fmt.Sprintf(`Period: %d`, now.Year()),
+		now,
 		cacheBookAccountID,
 	)
 	if err != nil {
-		tx.Rollback(ctx)
-		s.Logger.Error("failed to insert record: %v", zap.Error(err))
+		logger.Error("failed to insert record", zap.Error(err))
 		return err
 	}
 	err = tx.Commit(ctx)
 	return err
 }
 
-func (s *Storage) Withdrawal(ctx context.Context, user_id int64, amount decimal.Decimal, description *string) (err error) {
-	s.Logger.Debug("money withdrawal", zap.Int64(`userID`, user_id), zap.String(`amount`, amount.String()))
+func (s *Storage) Withdrawal(ctx context.Context, userID int64, amount decimal.Decimal, description *string) (err error) {
+	logger := s.Logger.With(zap.Int64("userID", userID))
+	logger.Debug("money withdrawal")
 
 	var now = time.Now()
 
@@ -157,76 +189,72 @@ func (s *Storage) Withdrawal(ctx context.Context, user_id int64, amount decimal.
 		return err
 	}
 
-	refreshSql := `REFRESH MATERIALIZED VIEW account_balances;`
-
-	_, err = tx.Exec(ctx, refreshSql)
-	if err != nil {
-		tx.Rollback(ctx)
-		s.Logger.Error("failed to refresh materialized view account_balances", zap.Error(err))
-		return err
-	}
-
-	selectSql := `SELECT balance FROM account_balances WHERE user_id = $1;`
-
-	var balance UserBalance
-	err = tx.QueryRow(ctx, selectSql, user_id).Scan(&balance.Balance)
-	if err != nil {
-		tx.Rollback(ctx)
-		s.Logger.Error("cannot return balance with specified ID")
-		if errors.Is(err, pgx.ErrNoRows) {
-			return errors.New("user does not exist")
+	defer func() {
+		if err != nil {
+			if errRollback := tx.Rollback(ctx); errRollback != nil {
+				logger.Error("error rolls back the transaction", zap.Error(err))
+			}
 		}
+	}()
+
+	var balance User
+	err = tx.QueryRow(ctx, updateRollUpTable, userID).Scan(&balance.Balance)
+	if err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) && pgErr.Code == pgerrcode.NotNullViolation {
+			logger.Error("error returning user balance with specified id: user does not exist", zap.Error(err))
+			return ErrUserAvailability
+		}
+		logger.Error("error returning user balance with specified id", zap.Error(err))
 		return err
 	}
 
 	if amount.GreaterThan(balance.Balance) {
-		tx.Rollback(ctx)
-		s.Logger.Error("insufficient funds on the user's account")
-		return err
+		logger.Error("insufficient funds on the user's account", zap.Error(ErrWithdrawal))
+		return ErrWithdrawal
 	}
 
-	firstInsertSql := `INSERT INTO posting (account_id, cb_journal, accounting_period, amount, date, description)
+	firstInsertExec := `INSERT INTO posting (account_id, cb_journal, accounting_period, amount, date, description)
 			VALUES ($1, $4, $5, -1 * $2, $3, $6);`
 
 	_, err = tx.Exec(
 		ctx,
-		firstInsertSql,
-		user_id,
+		firstInsertExec,
+		userID,
 		amount,
 		now.Format(time.RFC3339),
 		OperationTypeWithdrawal,
-		fmt.Sprintf(`Period: %d`, now.Year()),
+		now,
 		description,
 	)
 	if err != nil {
-		tx.Rollback(ctx)
-		s.Logger.Error("failed to insert record: %v", zap.Error(err))
+		logger.Error("failed to insert record", zap.Error(err))
 		return err
 	}
 
-	secondInsertSql := `INSERT INTO posting (account_id, cb_journal, accounting_period, amount, date)
+	secondInsertExec := `INSERT INTO posting (account_id, cb_journal, accounting_period, amount, date)
 			VALUES ($5, $3, $4, $1, $2);`
 
 	_, err = tx.Exec(
 		ctx,
-		secondInsertSql,
+		secondInsertExec,
 		amount,
 		now.Format(time.RFC3339),
 		OperationTypeWithdrawal,
-		fmt.Sprintf(`Period: %d`, now.Year()),
+		now,
 		cacheBookAccountID,
 	)
 	if err != nil {
-		tx.Rollback(ctx)
-		s.Logger.Error("failed to insert record: %v", zap.Error(err))
+		logger.Error("failed to insert record", zap.Error(err))
 		return err
 	}
 	err = tx.Commit(ctx)
 	return err
 }
 
-func (s *Storage) Transfer(ctx context.Context, user_id1, user_id2 int64, amount decimal.Decimal, description *string) error {
-	s.Logger.Debug("money transfer", zap.Int64("senderID", user_id1), zap.Int64("recipientID", user_id2), zap.String("amount", amount.String()))
+func (s *Storage) Transfer(ctx context.Context, sender, recipient int64, amount decimal.Decimal, description *string) error {
+	logger := s.Logger.With(zap.Int64("senderID", sender), zap.Int64("recipientID", recipient))
+	logger.Debug("money transfer")
 
 	var now = time.Now()
 
@@ -235,68 +263,65 @@ func (s *Storage) Transfer(ctx context.Context, user_id1, user_id2 int64, amount
 		return err
 	}
 
-	refreshSql := `REFRESH MATERIALIZED VIEW account_balances;`
-
-	_, err = tx.Exec(ctx, refreshSql)
-	if err != nil {
-		tx.Rollback(ctx)
-		s.Logger.Error("failed to refresh materialized view account_balances", zap.Error(err))
-		return err
-	}
-
-	selectSql := `SELECT balance FROM account_balances WHERE user_id = $1;`
-
-	var balance UserBalance
-	err = tx.QueryRow(ctx, selectSql, user_id1).Scan(&balance.Balance)
-	if err != nil {
-		tx.Rollback(ctx)
-		s.Logger.Error("cannot return balance with specified ID")
-		if errors.Is(err, pgx.ErrNoRows) {
-			return errors.New("user does not exist")
+	defer func() {
+		if err != nil {
+			if errRollback := tx.Rollback(ctx); errRollback != nil {
+				logger.Error("error rolls back the transaction")
+			}
 		}
+	}()
+
+	var balance User
+	err = tx.QueryRow(ctx, updateRollUpTable, sender).Scan(&balance.Balance)
+	if err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) && pgErr.Code == pgerrcode.NotNullViolation {
+			logger.Error("error returning user balance with specified id: user does not exist", zap.Error(err))
+			return ErrUserAvailability
+		}
+		logger.Error("error returning user balance with specified id", zap.Error(err))
 		return err
 	}
 
 	if amount.GreaterThan(balance.Balance) {
-		tx.Rollback(ctx)
-		s.Logger.Error("insufficient funds on the user's account")
-		return err
+		logger.Error("insufficient funds on the sender's account", zap.Error(ErrTransfer))
+		return ErrTransfer
 	}
 
-	firstInsertSql := `INSERT INTO posting (account_id, cb_journal, accounting_period, amount, date, addressee, description) 
+	firstInsertExec := `INSERT INTO posting (account_id, cb_journal, accounting_period, amount, date, addressee, description) 
 			VALUES ($1, $6, $4, -1 * $2, $3, $5, $7);`
 
 	_, err = tx.Exec(
 		ctx,
-		firstInsertSql,
-		user_id1,
+		firstInsertExec,
+		sender,
 		amount,
 		now.Format(time.RFC3339),
-		fmt.Sprintf(`Period: %d`, now.Year()),
-		user_id2,
+		now,
+		recipient,
 		OperationTypeTransfer,
 		description,
 	)
 	if err != nil {
-		tx.Rollback(ctx)
+		logger.Error("failed to insert record", zap.Error(err))
 		return err
 	}
 
-	secondInsertSql := `INSERT INTO posting (account_id, cb_journal, accounting_period, amount, date, addressee) 
+	secondInsertExec := `INSERT INTO posting (account_id, cb_journal, accounting_period, amount, date, addressee) 
 			VALUES ($1, $6, $4, $2, $3, $5);`
 
 	_, err = tx.Exec(
 		ctx,
-		secondInsertSql,
-		user_id2,
+		secondInsertExec,
+		recipient,
 		amount,
 		now.Format(time.RFC3339),
-		fmt.Sprintf(`Period: %d`, now.Year()),
-		user_id1,
+		now,
+		sender,
 		OperationTypeTransfer,
 	)
 	if err != nil {
-		tx.Rollback(ctx)
+		logger.Error("failed to insert record", zap.Error(err))
 		return err
 	}
 
@@ -304,43 +329,48 @@ func (s *Storage) Transfer(ctx context.Context, user_id1, user_id2 int64, amount
 	return err
 }
 
-func (s *Storage) ReadUserHistoryList(ctx context.Context, user_id int64, order string, limit, offset int64) ([]ReadUserHistoryResult, error) {
-	s.Logger.Debug("reading the user history list", zap.Int64("userID", user_id), zap.String("order", order), zap.Int64("limit", limit), zap.Int64("offset", offset))
+func (s *Storage) ReadUserHistoryList(
+	ctx context.Context,
+	userID int64,
+	order OrdBy,
+	limit, offset int64) ([]ReadUserHistoryResult, error) {
+	logger := s.Logger.With(zap.Int64("user_ID", userID))
+	logger.Debug("reading the user history list", zap.String("order", string(order)), zap.Int64("limit", limit), zap.Int64("offset", offset))
 
 	var sql string
 
-	amountSql := `SELECT account_id, cb_journal, amount, date, addressee, description FROM posting 
+	amountQuery := `SELECT account_id, cb_journal, amount, date, addressee, description FROM posting 
 		WHERE account_id = $1 ORDER BY amount LIMIT $2 OFFSET $3;`
 
-	dateSql := `SELECT account_id, cb_journal, amount, date, addressee, description FROM posting 
-	WHERE account_id = $1 ORDER BY date LIMIT $2 OFFSET $3;`
+	dateQuery := `SELECT account_id, cb_journal, amount, date, addressee, description FROM posting 
+		WHERE account_id = $1 ORDER BY date LIMIT $2 OFFSET $3;`
 
 	switch order {
-	case "amount":
-		sql = amountSql
-	case "date":
-		sql = dateSql
+	case OrderByAmount:
+		sql = amountQuery
+	case OrderByDate:
+		sql = dateQuery
 	}
 
 	rows, err := s.DB.Query(
 		ctx,
 		sql,
-		user_id,
+		userID,
 		limit,
 		offset,
 	)
 
 	if err != nil {
-		s.Logger.Error("cannot return user list with specified ID", zap.Error(err))
+		logger.Error("error returning user balance with specified id: user does not exist", zap.Error(err))
 		return nil, err
 	}
 
 	var rr []ReadUserHistoryResult
 	for rows.Next() {
 		var r ReadUserHistoryResult
-		err := rows.Scan(&r.AccountID, &r.CBjournal, &r.Amount, &r.Date, &r.Addressee, &r.Description)
+		err := rows.Scan(&r.AccountID, &r.CashBook, &r.Amount, &r.Date, &r.Addressee, &r.Description)
 		if err != nil {
-			s.Logger.Error("scanning row", zap.Error(err))
+			logger.Error("scanning row error", zap.Error(err))
 			return nil, err
 		}
 		r.Amount = decimal.New(r.Amount.IntPart(), -2)
