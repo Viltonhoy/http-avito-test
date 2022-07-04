7e1e3d609df41b3f5dab8fa9ced603f283b99f88
Nikita Ovsyannikov <nik_da_2000@mail.ru>
2022-07-04 12:35:04 +0300

    removed create materialized view;
    substitution for a roll-up table;

--- scripts/postgres/schema.sql
+++ scripts/postgres/schema.sql
@@ -4,25 +4,15 @@ CREATE TABLE posting(
 	id SERIAL PRIMARY KEY,
 	account_id bigint NOT NULL,
 	cb_journal operation_type NOT NULL,
-	accounting_period text NOT NULL,
+	accounting_period date NOT NULL,
 	amount bigint NOT NULL,
-	date date NOT NULL,
+	date timestamp with time zone NOT NULL,
 	addressee bigint,
 	description text 
 );
 
-create materialized view account_balances(
-user_id, balance	
-) as select
-	account_id,
-	sum(amount) 
-from posting 
-group by account_id
-with no data;
-
-create materialized view history_table(
-account_id, cb_journal, amount, date, addressee, description
-) as select
-	account_id, cb_journal, amount, date, addressee, description 
-from posting 
-with no data;
+CREATE TABLE balances(
+	balance bigint NOT NULL,
+    account_id bigint unique,
+    last_tx_id bigint NOT NULL
+);
