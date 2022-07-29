d4ffb823b3a05100c115b2345a720aa2e8763eff
Ovsyannikov Nikita <nik_da_2000@mail.ru>
2022-07-30 02:46:24 +0300

    set transaction isolation level serializable

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -42,9 +42,10 @@ const updateRollUpTable = `
 var ErrUserAvailability = errors.New("sender does not exist")
 
 var (
-	ErrWithdrawal = errors.New("not enough money to withdraw")
-	ErrTransfer   = errors.New("not enough money to transfer")
-	ErrNoUser     = errors.New("user does not exist")
+	ErrWithdrawal    = errors.New("not enough money to withdraw")
+	ErrTransfer      = errors.New("not enough money to transfer")
+	ErrNoUser        = errors.New("user does not exist")
+	ErrSerialization = errors.New("serialization level error")
 )
 
 // NewStore constructs Store instance with configured logger
@@ -103,6 +104,7 @@ func (s *Storage) ReadUserByID(ctx context.Context, userID int64) (u User, err e
 	}()
 
 	//query execution
+	//Roll-Up table updating and getting the user's balance
 	err = tx.QueryRow(ctx, updateRollUpTable, userID).Scan(&u.Balance)
 	if err != nil {
 		var pgErr *pgconn.PgError
@@ -123,6 +125,7 @@ func (s *Storage) ReadUserByID(ctx context.Context, userID int64) (u User, err e
 	}, err
 }
 
+// Deposit charge funds to the user's account
 func (s *Storage) Deposit(ctx context.Context, userID int64, amount decimal.Decimal) (err error) {
 	logger := s.Logger.With(zap.Int64(`user_ID`, userID))
 	logger.Debug("money deposit")
@@ -142,6 +145,7 @@ func (s *Storage) Deposit(ctx context.Context, userID int64, amount decimal.Deci
 		}
 	}()
 
+	// charge funds to the user's account
 	firstInsertExec := `INSERT INTO posting (account_id, cb_journal, accounting_period, amount, date)
 			VALUES ($1, $4, $5, $2, $3);`
 
@@ -159,6 +163,7 @@ func (s *Storage) Deposit(ctx context.Context, userID int64, amount decimal.Deci
 		return err
 	}
 
+	// notes the deposit in the cache book
 	secondInsertExec := `INSERT INTO posting (account_id, cb_journal, accounting_period, amount, date)
 			VALUES ($5, $3, $4, -1 * $1, $2);`
 
@@ -179,13 +184,15 @@ func (s *Storage) Deposit(ctx context.Context, userID int64, amount decimal.Deci
 	return err
 }
 
+// withdrawal deducts money from the user's account
 func (s *Storage) Withdrawal(ctx context.Context, userID int64, amount decimal.Decimal, description *string) (err error) {
 	logger := s.Logger.With(zap.Int64("userID", userID))
 	logger.Debug("money withdrawal")
 
 	var now = time.Now()
 
-	tx, err := s.DB.Begin(ctx)
+	// start transaction with transaction isolation level options
+	tx, err := s.DB.BeginTx(ctx, pgx.TxOptions{IsoLevel: pgx.Serializable})
 	if err != nil {
 		return err
 	}
@@ -202,6 +209,10 @@ func (s *Storage) Withdrawal(ctx context.Context, userID int64, amount decimal.D
 	err = tx.QueryRow(ctx, updateRollUpTable, userID).Scan(&balance.Balance)
 	if err != nil {
 		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) && pgErr.Code == pgerrcode.SerializationFailure {
+			logger.Warn("transaction isolation level error", zap.Error(err))
+			return ErrSerialization
+		}
 		if errors.As(err, &pgErr) && pgErr.Code == pgerrcode.NotNullViolation {
 			logger.Error("error returning user balance with specified id: user does not exist", zap.Error(err))
 			return ErrUserAvailability
@@ -210,11 +221,13 @@ func (s *Storage) Withdrawal(ctx context.Context, userID int64, amount decimal.D
 		return err
 	}
 
+	// checking the condition that the balance is greater than or equal to the amount
 	if amount.GreaterThan(balance.Balance) {
 		logger.Error("insufficient funds on the user's account", zap.Error(ErrWithdrawal))
 		return ErrWithdrawal
 	}
 
+	// deducts money from the user's account
 	firstInsertExec := `INSERT INTO posting (account_id, cb_journal, accounting_period, amount, date, description)
 			VALUES ($1, $4, $5, -1 * $2, $3, $6);`
 
@@ -233,6 +246,7 @@ func (s *Storage) Withdrawal(ctx context.Context, userID int64, amount decimal.D
 		return err
 	}
 
+	// notes the withdrawal in the cache book
 	secondInsertExec := `INSERT INTO posting (account_id, cb_journal, accounting_period, amount, date)
 			VALUES ($5, $3, $4, $1, $2);`
 
@@ -253,13 +267,15 @@ func (s *Storage) Withdrawal(ctx context.Context, userID int64, amount decimal.D
 	return err
 }
 
+// transfer performs the transfer of money from sender to recipient
 func (s *Storage) Transfer(ctx context.Context, sender, recipient int64, amount decimal.Decimal, description *string) error {
 	logger := s.Logger.With(zap.Int64("senderID", sender), zap.Int64("recipientID", recipient))
 	logger.Debug("money transfer")
 
 	var now = time.Now()
 
-	tx, err := s.DB.Begin(ctx)
+	// start transaction with transaction isolation level options
+	tx, err := s.DB.BeginTx(ctx, pgx.TxOptions{IsoLevel: pgx.Serializable})
 	if err != nil {
 		return err
 	}
@@ -276,6 +292,10 @@ func (s *Storage) Transfer(ctx context.Context, sender, recipient int64, amount 
 	err = tx.QueryRow(ctx, updateRollUpTable, sender).Scan(&balance.Balance)
 	if err != nil {
 		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) && pgErr.Code == pgerrcode.SerializationFailure {
+			logger.Warn("transaction isolation level error", zap.Error(err))
+			return ErrSerialization
+		}
 		if errors.As(err, &pgErr) && pgErr.Code == pgerrcode.NotNullViolation {
 			logger.Error("error returning user balance with specified id: user does not exist", zap.Error(err))
 			return ErrUserAvailability
@@ -284,11 +304,13 @@ func (s *Storage) Transfer(ctx context.Context, sender, recipient int64, amount 
 		return err
 	}
 
+	// checking the condition that the balance is greater than or equal to the amount
 	if amount.GreaterThan(balance.Balance) {
 		logger.Error("insufficient funds on the sender's account", zap.Error(ErrTransfer))
 		return ErrTransfer
 	}
 
+	// deducts money from the sender account
 	firstInsertExec := `INSERT INTO posting (account_id, cb_journal, accounting_period, amount, date, addressee, description) 
 			VALUES ($1, $6, $4, -1 * $2, $3, $5, $7);`
 
@@ -299,7 +321,7 @@ func (s *Storage) Transfer(ctx context.Context, sender, recipient int64, amount 
 		amount,
 		now.Format(time.RFC3339),
 		now,
-		recipient,
+		sender,
 		OperationTypeTransfer,
 		description,
 	)
@@ -308,6 +330,7 @@ func (s *Storage) Transfer(ctx context.Context, sender, recipient int64, amount 
 		return err
 	}
 
+	// charge funds to the recipient account
 	secondInsertExec := `INSERT INTO posting (account_id, cb_journal, accounting_period, amount, date, addressee) 
 			VALUES ($1, $6, $4, $2, $3, $5);`
 
@@ -318,7 +341,7 @@ func (s *Storage) Transfer(ctx context.Context, sender, recipient int64, amount 
 		amount,
 		now.Format(time.RFC3339),
 		now,
-		sender,
+		recipient,
 		OperationTypeTransfer,
 	)
 	if err != nil {
@@ -330,6 +353,7 @@ func (s *Storage) Transfer(ctx context.Context, sender, recipient int64, amount 
 	return err
 }
 
+// ReadUserHistoryList returns the user's sorted transaсtion history
 func (s *Storage) ReadUserHistoryList(
 	ctx context.Context,
 	userID int64,
