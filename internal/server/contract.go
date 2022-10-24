746769bc9f90b5c6de1b9330f5dede24f95a51d1
Ovsyannikov Nikita <nik_da_2000@mail.ru>
2022-10-24 19:25:15 +0300

    adding new methods to the interface;

--- internal/server/contract.go
+++ internal/server/contract.go
@@ -12,8 +12,12 @@ type Storager interface {
 	ReadUserByID(context.Context, int64) (storage.User, error)
 	Deposit(context.Context, int64, decimal.Decimal) error
 	Withdrawal(context.Context, int64, decimal.Decimal, *string) error
-	Transfer(ctx context.Context, user_id1, user_id2 int64, amount decimal.Decimal, description *string) error
+	Transfer(ctx context.Context, user_id1, user_id2 int64, amount decimal.Decimal, description *string, options ...storage.TxOption) (int64, int64, error)
 	ReadUserHistoryList(ctx context.Context, user_id int64, order storage.OrdBy, limit, offset int64) ([]storage.ReadUserHistoryResult, error)
+	Reservation(ctx context.Context, UserId int64, ServiceId int64, OrderId int64, Price decimal.Decimal, description *string) error
+	Revenue(ctx context.Context, UserId int64, ServiceId int64, OrderId int64, Sum decimal.Decimal, description *string) error
+	Unreservation(ctx context.Context, UserId int64, ServiceId int64, OrderId int64, description *string) error
+	MonthlyReport(ctx context.Context, year int64, month int64) ([][]string, error)
 }
 
 type Exchanger interface {
