4767ee369315a9c5367937ef5e63acdab8a74d5d
Ovsyannikov Nikita <nik_da_2000@mail.ru>
2022-10-24 19:31:12 +0300

    adding new tables;

--- scripts/postgres/schema/schema.sql
+++ scripts/postgres/schema/schema.sql
@@ -1,7 +1,9 @@
 create type operation_type as enum('deposit', 'withdrawal', 'transfer');
 
+create type expenses_type as enum('reservation', 'unreservation');
+
 CREATE TABLE posting(
-	id SERIAL PRIMARY KEY,
+	id BIGSERIAL PRIMARY KEY,
 	account_id bigint NOT NULL,
 	cb_journal operation_type NOT NULL,
 	accounting_period date NOT NULL,
@@ -15,4 +17,22 @@ CREATE TABLE balances(
 	balance bigint NOT NULL,
     account_id bigint unique,
     last_tx_id bigint NOT NULL
+);
+
+CREATE TABLE deferred_expenses(
+	account_id bigint NOT NULL, 
+	service_id bigint NOT NULL,
+	order_id bigint NOT NULL,
+	operation expenses_type NOT NULL, 
+	price bigint NOT NULL,
+	tx_id      bigint references posting (id),
+	UNIQUE (operation, order_id)
+);
+
+CREATE TABLE consolidated_report(
+	account_id bigint NOT NULL, 
+	service_id bigint NOT NULL,
+	order_id bigint NOT NULL unique,
+	sum bigint NOT NULL,
+	tx_id      bigint references posting (id)
 );
